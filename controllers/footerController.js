e94b05e837ed4e6dcaae05dcc66c574e977151ad
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-11-23 00:42:55 +0100

    update .env with Mailtrap credentials, update footerController with the correct transporter credentials, update index.js with the footer routes

--- controllers/footerController.js
+++ controllers/footerController.js
@@ -1,9 +1,10 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const filmRoutes = require('./routes/filmRoutes.js');
 const memeRoutes = require('./routes/memeRoutes.js');
 const quizRoutes = require('./routes/quizRoutes.js');
 const commentRoutes = require('./routes/commentRoutes');
+const footerRoutes = require('./routes/footerRoutes');
 const mainRoutes = require('./routes/mainRoutes'); 
 
 app.use(express.json());
@@ -45,6 +46,7 @@ app.use('/', imageRoutes);
 app.use('/', memeRoutes);
 app.use('/', quizRoutes);
 app.use('/', commentRoutes);
+app.use('/', footerRoutes);
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
   res.status(500).json({ message: 'An unexpected error occurred.' });
