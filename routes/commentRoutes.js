a7c6f08ebd88993b831fa4d3cb077b0376fa15d0
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-12-02 16:55:19 +0100

    update the commentRoutes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { addComment, getComments, editComment, deleteComment } = require('../controllers/commentController');
+const verifyToken = require('../middlewares/auth')
 const router = express.Router();
 
-router.post('/comments', addComment);
+router.post('/comments', verifyToken, addComment);
 
 router.get('/comments/:contentType/:contentId', getComments);
 
-router.put('/comments/:commentId', editComment);
+router.put('/comments/:commentId', verifyToken, editComment);
 
-router.delete('/comments/:commentId', deleteComment);
+router.delete('/comments/:commentId', verifyToken, deleteComment);
 
 module.exports = router;
